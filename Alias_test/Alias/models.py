4e9f50745b23f6ed92643a53120f37b47bf8e4b9
Alex Shu <45352474+vrednyydragon@users.noreply.github.com>
2021-02-11 01:19:53 +0200

    init

--- Alias_test/Alias/models.py
+++ Alias_test/Alias/models.py
@@ -4,7 +4,7 @@ class Alias(models.Model):
     alias = models.TextField(blank=True, null=True)
     target = models.TextField(max_length=24)
     start = models.DateTimeField()
-    end = models.DateTimeField(default='9999-12-31 00:00:00.000000+00:00')
+    end = models.DateTimeField(default='9999-12-31 00:00:00.000001+00:00')
 
     def __str__(self):
         return f"{self.alias}- {self.target} - {self.start} - {self.end}"
